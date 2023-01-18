fca6902d884d5c4e43c4b14569efe86c1f5ebe23
Don Pablo el de Felicitas <pabloalegrejuan@gmail.com>
2023-01-18 00:38:07 +0000

    vaya

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import "./App.css";
  */
 
 export default function App() {
+  console.log("holaaaa",process.env.PUBLIC_URL)
   return (
     <div className="app min-h-screen text-blue-200 flex items-center flex-col p-20">
       <div className="mb-10 grid grid-cols-4 grid-rows-2 w-1/2 mx-auto">
